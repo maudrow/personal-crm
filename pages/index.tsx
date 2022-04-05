b5e4dcba09d17a212b01d05218c8dac499d5dc9e
Audrow Nash <audrow@hey.com>
2022-04-05 10:44:36 -0700

    Setup supabase with credentials
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react'
+import { supabase } from '../utils/supabase-client'
+import Auth from '../components/Auth'
+import Account from '../components/Account'
+import type { Session } from '@supabase/supabase-js'
+
 export default function Home() {
+  const [session, setSession] = useState<Session|null>()
+
+  useEffect(() => {
+    const session = supabase.auth.session()
+    setSession(session)
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session ? <Auth /> : <Account key={session.user?.id} session={session} />}
     </div>
   )
 }
